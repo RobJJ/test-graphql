acbd3e97efa74f8f2b817f7a2bc23b872dcae5f7
RobJ <robjjohnsenwork@gmail.com>
2023-02-13 12:21:12 +0700

    Initial setup

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+// this function will help us build a schema using the type system
+const { buildSchema } = require("graphql");
+// middleware function
+const { graphqlHTTP } = require("express-graphql");
+
+// start with special query type, root type.. defines enter point for all queries..
+// we are determining shape of data
+const schema = buildSchema(`
+type Query {
+    description: String
+    price: Float
+}
+`);
+
+const root = {
+  description: "Red Shoe",
+  price: 42.12,
+};
+
+const app = express();
+//
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+  })
+);
+
+app.listen(3000, () => {
+  console.log(`Running graphQL server...`);
+});
