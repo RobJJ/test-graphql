fee6e4a7af6f136ec5a0f34b1f137855394bcc99
RobJ <robjjohnsenwork@gmail.com>
2023-02-14 11:06:38 +0700

    setup of graphql-tools, and use of functions

--- server.js
+++ server.js
@@ -1,21 +1,64 @@
 const express = require("express");
+const path = require("path");
 // this function will help us build a schema using the type system
-const { buildSchema } = require("graphql");
+// const { buildSchema } = require("graphql");
 // middleware function
 const { graphqlHTTP } = require("express-graphql");
+// graphQL functions taken from package
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
 
+// const schemaText = `
+// type Query {
+//     products: [Product]
+//     orders: [Order]
+// }
+// `;
+// ** look into any directorys../* match any files with ____
+// use build in path module and join func
+// we want this loadFile func to look at our server directory specifically so we need to use the __dirname
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+// this makeExecutableSchema is replacing the buildSchema function we used earlier to set the schema
+const schema = makeExecutableSchema({
+  // typeDefs are like schemas
+  typeDefs: [schemaText],
+});
 // start with special query type, root type.. defines enter point for all queries..
 // we are determining shape of data
-const schema = buildSchema(`
-type Query {
-    description: String
-    price: Float
-}
-`);
+// [Product], will be a list of these types..
+// make fields required by using ! after
+// ID is a special graphQL type we can use, almost like a string but more explicit
+// const schema = buildSchema();
 
 const root = {
-  description: "Red Shoe",
-  price: 42.12,
+  products: [
+    {
+      id: "redshoe",
+      description: "Red Shoe",
+      price: 42.54,
+    },
+    {
+      id: "bluejean",
+      description: "Blue Jeans",
+      price: 12.94,
+    },
+  ],
+  orders: [
+    {
+      date: "2005-05-05",
+      subtotal: 90.22,
+      items: [
+        {
+          product: {
+            id: "redshoe",
+            description: "Old Red Shoe",
+            price: 45.22,
+          },
+          quantity: 2,
+        },
+      ],
+    },
+  ],
 };
 
 const app = express();
